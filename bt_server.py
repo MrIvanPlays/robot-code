8b91131fb727ea7bcaafe54b013c218695834d7e
Ivan Pekov <ivan@mrivanplays.com>
2022-12-21 17:03:18 +0200

    if this shit doesn't work how i want it to work, then ig nothing can

--- bt_server.py
+++ bt_server.py
@@ -19,30 +19,40 @@ bluetooth.advertise_service(server_sock,
                             )
 
 print("Listening for incoming connections")
-while True:
-    client_sock, address = server_sock.accept()
-    print("Connection from ", address)
+client_sock, address = server_sock.accept()
+print("Connection from ", address)
 
-    received = client_sock.recv(1024).decode("utf-8").splitlines()
-    duty_cycle = received[1]
-    movement = received[0]
-    print("read duty_cycle of " + duty_cycle + " with movement type of " + movement)
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if not data:
+            break
+        received = data.decode("utf-8").splitlines()
+        duty_cycle = received[1]
+        movement = received[0]
+        print("read duty_cycle of " + duty_cycle + " with movement type of " + movement)
 
-    if movement == 'forward':
-        forward_cmd = subprocess.run(["rcontr", "control", "--movement forward", "--duty-cycle " + duty_cycle])
-        print("Exit code: %d" % forward_cmd.returncode)
-    if movement == 'backward':
-        backward_cmd = subprocess.run(["rcontr", "control", "--movement backward", "--duty-cycle " + duty_cycle])
-        print("Exit code: %d" % backward_cmd.returncode)
-    if movement == 'left':
-        left = subprocess.run(["rcontr", "control", "--movement left",  "--duty-cycle " + duty_cycle])
-        print("Exit code: %d" % left.returncode)
-    if movement == 'right':
-        right = subprocess.run(["rcontr", "control", "--movement right", "--duty-cycle " + duty_cycle])
-        print("Exit code: %d" % right.returncode)
-    if movement == 'parallelLeft':
-        pLeft = subprocess.run(["rcontr", "control", "--movement parallel_left", "--duty-cycle " + duty_cycle])
-        print("Exit code: %d" % pLeft.returncode)
-    if movement == 'parallelRight':
-        pRight = subprocess.run(["rcontr", "control", "--movement parallel_right", "--duty-cycle " + duty_cycle])
-        print("Exit code: %d" % pRight.returncode)
+        if movement == 'forward':
+            forward_cmd = subprocess.run(["rcontr", "control", "--movement forward", "--duty-cycle " + duty_cycle])
+            print("Exit code: %d" % forward_cmd.returncode)
+        if movement == 'backward':
+            backward_cmd = subprocess.run(["rcontr", "control", "--movement backward", "--duty-cycle " + duty_cycle])
+            print("Exit code: %d" % backward_cmd.returncode)
+        if movement == 'left':
+            left = subprocess.run(["rcontr", "control", "--movement left",  "--duty-cycle " + duty_cycle])
+            print("Exit code: %d" % left.returncode)
+        if movement == 'right':
+            right = subprocess.run(["rcontr", "control", "--movement right", "--duty-cycle " + duty_cycle])
+            print("Exit code: %d" % right.returncode)
+        if movement == 'parallelLeft':
+            pLeft = subprocess.run(["rcontr", "control", "--movement parallel_left", "--duty-cycle " + duty_cycle])
+            print("Exit code: %d" % pLeft.returncode)
+        if movement == 'parallelRight':
+            pRight = subprocess.run(["rcontr", "control", "--movement parallel_right", "--duty-cycle " + duty_cycle])
+            print("Exit code: %d" % pRight.returncode)
+except OSError:
+    pass
+
+print("Disconnected")
+client_sock.close()
+server_sock.close()
