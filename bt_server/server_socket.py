f4dc9dd9fe8d13b6e8e4f39fe89398561563a112
Ivan Pekov <ivan@mrivanplays.com>
2022-12-21 16:25:52 +0200

    properly read content coming from android app

--- bt_server/server_socket.py
+++ bt_server/server_socket.py
@@ -3,20 +3,21 @@ import subprocess
 
 
 def readlines(socket):
-    buffer = socket.recv(1024)
-    buffering = True
-    while buffering:
-        if "\n".encode() in buffer:
-            (line, buffer) = buffer.split("\n".encode(), 1)
-            yield line
-        else:
-            more = socket.recv(1024)
-            if not more:
-                buffering = False
-            else:
-                buffer += more
-    if buffer:
-        yield buffer
+    # buffer = socket.recv(1024)
+    # buffering = True
+    # while buffering:
+    #     if "\n".encode() in buffer:
+    #         (line, buffer) = buffer.split("\n".encode(), 1)
+    #         yield line
+    #     else:
+    #         more = socket.recv(1024)
+    #         if not more:
+    #             buffering = False
+    #         else:
+    #             buffer += more
+    # if buffer:
+    #     yield buffer
+    return socket.recv(1024).decode("utf-8").splitlines()
 
 
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -39,34 +40,35 @@ print("Listening for incoming connections")
 client_sock, address = server_sock.accept()
 print("Connection from ", address)
 
-duty_cycle = ""
-movement = ""
+received = readlines(client_sock)
+duty_cycle = received[1]
+movement = received[0]
+print("read duty_cycle of " + duty_cycle + " with movement type of " + movement)
+# index = 0
+# for line in readlines(client_sock):
+#     print("Received [%s]" % line)
+#     if index == 0:
+#         movement = line
+#     if index == 1:
+#         duty_cycle = line
+#     index = index + 1
 
-index = 0
-for line in readlines(client_sock):
-    print("Received [%s]" % line)
-    if index == 0:
-        movement = line
-    if index == 1:
-        duty_cycle = line
-    index = index + 1
-
-if movement == b'forward':
+if movement == 'forward':
     forward_cmd = subprocess.run(["rcontr", "control", "--movement forward", "--duty-cycle " + duty_cycle])
     print("Exit code: %d" % forward_cmd.returncode)
-if movement == b'backward':
+if movement == 'backward':
     backward_cmd = subprocess.run(["rcontr", "control", "--movement backward", "--duty-cycle " + duty_cycle])
     print("Exit code: %d" % backward_cmd.returncode)
-if movement == b'left':
+if movement == 'left':
     left = subprocess.run(["rcontr", "control", "--movement left",  "--duty-cycle " + duty_cycle])
     print("Exit code: %d" % left.returncode)
-if movement == b'right':
+if movement == 'right':
     right = subprocess.run(["rcontr", "control", "--movement right", "--duty-cycle " + duty_cycle])
     print("Exit code: %d" % right.returncode)
-if movement == b'parallelLeft':
+if movement == 'parallelLeft':
     pLeft = subprocess.run(["rcontr", "control", "--movement parallel_left", "--duty-cycle " + duty_cycle])
     print("Exit code: %d" % pLeft.returncode)
-if movement == b'parallelRight':
+if movement == 'parallelRight':
     pRight = subprocess.run(["rcontr", "control", "--movement parallel_right", "--duty-cycle " + duty_cycle])
     print("Exit code: %d" % pRight.returncode)
 
